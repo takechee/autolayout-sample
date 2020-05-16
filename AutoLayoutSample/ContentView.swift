b7eb1dc067aa8a0d67fc3279b07d5d609d4dfc44
takechee <takechee14@gmail.com>
2020-05-16 11:16:52 +0900

    プロジェクト作成

---
+++ AutoLayoutSample/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  AutoLayoutSample
+//
+//  Created by Ryo Takeuchi on 2020/05/16.
+//  Copyright © 2020 takechee. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
